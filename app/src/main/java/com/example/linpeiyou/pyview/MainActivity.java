b9dab582d223cf8182c6546de686a60eaf995fad
linpeiyou <linpeiyou@u51.com>
2017-09-17 22:52:19 +0800

    完成了自定义CircleView

--- app/src/main/java/com/example/linpeiyou/pyview/MainActivity.java
+++ app/src/main/java/com/example/linpeiyou/pyview/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.linpeiyou.pyview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findView();
+        initView();
+    }
+
+    private void findView() {
+        btnCircle = (Button) findViewById(R.id.btn_circle);
+    }
+
+    private void initView() {
+        btnCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CircleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
